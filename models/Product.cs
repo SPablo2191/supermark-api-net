f9500bb680f78a6e478a366e53532547d31ad764
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-06-05 23:25:06 -0300

    feat: add product model

---
+++ models/Product.cs
@@ -0,0 +1,10 @@
+public class Product{
+    public Guid Id {get; set;}
+    public string? name {get; set;}
+    public decimal? price {get; set;}
+    public string? brand {get; set;}
+    public DateTime? expirationDate {get; set;}
+
+    public DateTime? registerDate {get; set;}
+
+}
